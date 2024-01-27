8f11abf6a69aab7a34764740e414aa06894c7f6b
tomek-i <tomek.iwainski@gmail.com>
2024-01-28 01:03:55 +1100

    Added character component

---
+++ src/components/Character/Character.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Character } from ".";
+
+
+const meta: Meta<typeof Character> = {
+  component: Character,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Character>;
+
+export const Primary: Story = {
+  render: () => <Character />,
+};
---
+++ src/components/Character/Character.tsx
@@ -0,0 +1,7 @@
+export interface CharacterProps extends React.PropsWithChildren {}
+
+export const Character: React.FC<CharacterProps > = ({}) => {
+    return (
+        <div>Character</div>
+    );
+};
