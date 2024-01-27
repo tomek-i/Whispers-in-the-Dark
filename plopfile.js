b9d1d02ba82e9b612129708f511e33fb0cc3eb7d
tomek-i <tomek.iwainski@gmail.com>
2024-01-28 02:05:44 +1100

    Update plopfile.js

--- plopfile.js
+++ plopfile.js
@@ -1,44 +1,108 @@
-module.exports = (plop) => {
-  plop.setGenerator("component", {
-    description: "Create a component",
-    // User input prompts provided as arguments to the template
+module.exports = async function (plop) {
+  plop.setDefaultInclude({ generators: true })
+  plop.setGenerator("context", {
+    description: "⚛ react context",
     prompts: [
       {
-        // Raw text input
         type: "input",
-        // Variable name for this input
         name: "name",
-        // Prompt to display on command line
-        message: "What is your component name?",
+        message: "context name",
       },
     ],
     actions: [
       {
-        // Add a new file
-        type: "add",
-        // Path for the new file
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
-        // Handlebars template used to generate content of new file
-        templateFile: ".plop-templates/component/stories.tsx.hbs",
-        skipIfExists: true,
+        type: "append",
+        path: "src/context/index.ts",
+        templateFile: ".templates/react/context/templates/index.hbs",
       },
       {
-        // Add a new file
         type: "add",
-        // Path for the new file
+        path: "src/context/{{pascalCase name}}.tsx",
+        templateFile: ".templates/react/context/templates/context.hbs",
+      },
+      {
+        type: "append",
+        path: "src/hooks/index.ts",
+        templateFile: ".templates/react/hooks/templates/index.hbs",
+      },
+      {
+        type: "add",
+        path: "src/hooks/use{{pascalCase name}}/index.ts",
+        templateFile: ".templates/react/hooks/templates/index.hbs",
+      },
+      {
+        type: "add",
+        path: "src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.ts",
+        templateFile: ".templates/react/hooks/templates/hook.hbs",
+      },
+      {
+        type: "add",
+        path: "src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.test.ts",
+        templateFile: ".templates/react/hooks/templates/test.hbs",
+      },
+    ],
+  })
+  plop.setGenerator("hook", {
+    description: "⚛ react hook",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "hook name",
+      },
+    ],
+    actions: [
+      {
+        type: "append",
+        path: "src/hooks/index.ts",
+        templateFile: ".templates/react/hooks/templates/hooks.hbs",
+      },
+      {
+        type: "add",
+        path: "src/hooks/{{camelCase name}}/index.ts",
+        templateFile: ".templates/react/hooks/templates/index.hbs",
+      },
+      {
+        type: "add",
+        path: "src/hooks/{{camelCase name}}/{{camelCase name}}.ts",
+        templateFile: ".templates/react/hooks/templates/hook.hbs",
+      },
+      {
+        type: "add",
+        path: "src/hooks/{{camelCase name}}/{{camelCase name}}.test.ts",
+        templateFile: ".templates/react/hooks/templates/test.hbs",
+      },
+    ],
+  })
+  plop.setGenerator("component", {
+    description: "⚛ react component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "component name",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
         path: "src/components/{{pascalCase name}}/index.ts",
-        // Handlebars template used to generate content of new file
-        templateFile: ".plop-templates/component/index.ts.hbs",
-        skipIfExists: true,
+        templateFile: ".templates/react/component/templates/index.hbs",
       },
       {
-        // Add a new file
         type: "add",
-        // Path for the new file
         path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
-        // Handlebars template used to generate content of new file
-        templateFile: ".plop-templates/component/Component.tsx.hbs",
-        skipIfExists: true,
+        templateFile: ".templates/react/component/templates/component.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
+        templateFile: ".templates/react/component/templates/test.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.variants.ts",
+        templateFile: ".templates/react/component/templates/variants.hbs",
       },
     ],
   })
