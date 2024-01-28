c1474f32d917261e789b4f3d05def7eca82897ea
tomek-i <tomek.iwainski@gmail.com>
2024-01-29 10:26:08 +1100

    Update AdminMainNavigation.test.tsx

--- src/components/AdminMainNavigation/AdminMainNavigation.test.tsx
+++ src/components/AdminMainNavigation/AdminMainNavigation.test.tsx
@@ -1,7 +1,7 @@
-import { AdminMainNavigation } from "components/AdminMainNavigation"
 import { render, screen } from "@testing-library/react"
+import { AdminMainNavigation } from "src/components/AdminMainNavigation"
 
 test("renders", () => {
-    render(<AdminMainNavigation />)
-    expect(screen.getByText("AdminMainNavigation"))
+  render(<AdminMainNavigation />)
+  expect(screen.getByText("AdminMainNavigation")).toBeInTheDocument()
 })
