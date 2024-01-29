aa5d0bb24c3fc44952f0150f06e52b8d5dc96d12
tomek-i <tomek.iwainski@gmail.com>
2024-01-29 23:29:19 +1100

    Update server.env.ts

--- src/env/server.env.ts
+++ src/env/server.env.ts
@@ -3,21 +3,9 @@ import { z } from "zod"
 
 export const env = createEnv({
   server: {
-    FIREBASE_API_KEY: z.string(),
-    FIREBASE_AUTH_DOMAIN: z.string(),
-    FIREBASE_PROJECT_ID: z.string(),
-    FIREBASE_STORAGE_BUCKET: z.string(),
-    FIREBASE_MESSAGING_SENDER_ID: z.string(),
-    FIREBASE_APP_ID: z.string(),
-    FIREBASE_MEASUREMENT_ID: z.string(),
+    FIREBASE_DATABASE_URL: z.string().url(),
   },
   runtimeEnv: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
   },
 })
