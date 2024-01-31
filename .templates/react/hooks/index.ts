548c6e8201ba95d1bd919f0e453426f0c72694fa
tomek-i <tomek.iwainski@gmail.com>
2024-02-01 00:30:30 +1100

    Update index.ts

--- .templates/react/hooks/index.ts
+++ .templates/react/hooks/index.ts
@@ -1,32 +1,32 @@
 export const hook = {
-    description: "⚛ react hook",
-    prompts: [
-        {
-            type: "input",
-            name: "name",
-            message: "hook name",
-        },
-    ],
-    actions: [
-        {
-            type: "append",
-            path: "src/hooks/index.ts",
-            templateFile: "./templates/hooks.hbs",
-        },
-        {
-            type: "add",
-            path: "src/hooks/{{camelCase name}}/index.ts",
-            templateFile: "./templates/index.hbs",
-        },
-        {
-            type: "add",
-            path: "src/hooks/{{camelCase name}}/{{camelCase name}}.ts",
-            templateFile: "./templates/hook.hbs",
-        },
-        {
-            type: "add",
-            path: "src/hooks/{{camelCase name}}/{{camelCase name}}.test.ts",
-            templateFile: "./templates/test.hbs",
-        },
-    ],
+  description: "⚛ react hook",
+  prompts: [
+    {
+      type: "input",
+      name: "name",
+      message: "hook name",
+    },
+  ],
+  actions: [
+    {
+      type: "append",
+      path: "src/hooks/index.ts",
+      templateFile: "./templates/hooks.hbs",
+    },
+    {
+      type: "add",
+      path: "src/hooks/use{{camelCase name}}/index.ts",
+      templateFile: "./templates/index.hbs",
+    },
+    {
+      type: "add",
+      path: "src/hooks/use{{camelCase name}}/use{{camelCase name}}.ts",
+      templateFile: "./templates/hook.hbs",
+    },
+    {
+      type: "add",
+      path: "src/hooks/use{{camelCase name}}/use{{camelCase name}}.test.ts",
+      templateFile: "./templates/test.hbs",
+    },
+  ],
 }
