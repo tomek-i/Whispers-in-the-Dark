021b77e86d170c572154ebffb81d7c32761900ab
tomek-i <tomek.iwainski@gmail.com>
2024-02-01 20:59:36 +1100

    added database app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-// import { getDatabase } from "firebase/database"
-import { env as clientEnv } from "@/env/client.env"
+import { getDatabase } from "firebase/database"
+import { env } from "@/env/client.env"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,22 +9,24 @@ import { env as clientEnv } from "@/env/client.env"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: clientEnv.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: clientEnv.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: clientEnv.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: clientEnv.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: clientEnv.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: clientEnv.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: clientEnv.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  // databaseURL: serverEnv.FIREBASE_DATABASE_URL,
+  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 }
 // Initialize Firebase
 const firebase_app = initializeApp(firebaseConfig)
 // const firebase_analytics = getAnalytics(firebase_app)
-// const database = getDatabase(firebase_app)
+
+// Initialize Realtime Database and get a reference to the service
+const firebase_database = getDatabase(firebase_app)
 
 export const firebase = {
   app: firebase_app,
   // analytics: firebase_analytics,
-  // db: database,
+  database: firebase_database,
 }
