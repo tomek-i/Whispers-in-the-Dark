da9b6a43a14c46cd43f32d4a2470af852b4b5f5c
tomek-i <tomek.iwainski@gmail.com>
2024-02-01 23:42:09 +1100

    testing firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,70 @@
+"use client"
+import { getToken, onMessage } from "firebase/messaging"
 import { Metadata } from "next"
 import Image from "next/image"
+import { useEffect } from "react"
+import { toast, ToastContainer } from "react-toastify"
 import { Button } from "@/components/Button/Button"
 import { MainNavigaion } from "@/components/MainNavigaion"
+import "react-toastify/dist/ReactToastify.css"
+import { env } from "@/env/client.env"
+import { firebase } from "@/lib/firebase"
 
-export const metadata: Metadata = {
-  title: "Whispers in the Dark - Tomek Iwainski",
-  twitter: {
-    card: "summary_large_image",
-  },
-  openGraph: {
-    url: "https://github.com/tomek-i/whispers-in-the-dark/",
-    images: [
-      {
-        width: 2000,
-        height: 1120,
-        url: "https://github.com/tomek-i/whispers-in-the-dark/main/.github/assets/project-logo.jpg",
-      },
-    ],
-  },
-}
+// export const metadata: Metadata = {
+//   title: "Whispers in the Dark - Tomek Iwainski",
+//   twitter: {
+//     card: "summary_large_image",
+//   },
+//   openGraph: {
+//     url: "https://github.com/tomek-i/whispers-in-the-dark/",
+//     images: [
+//       {
+//         width: 2000,
+//         height: 1120,
+//         url: "https://github.com/tomek-i/whispers-in-the-dark/main/.github/assets/project-logo.jpg",
+//       },
+//     ],
+//   },
+// }
 
 export default function Web() {
+  // onMessage(messaging, (payload) => {
+  //   toast(payload.notification)
+  // })
+
+  const onMessageListener = () =>
+    new Promise((resolve) => {
+      onMessage(firebase.messaging, (payload) => {
+        console.log("payload", payload)
+        resolve(payload)
+      })
+    })
+
+  onMessageListener().then((payload) => {
+    console.log({ payload })
+  })
+  async function requestPermission() {
+    //requesting permission using Notification API
+    const permission = await Notification.requestPermission()
+
+    if (permission === "granted") {
+      const token = await getToken(firebase.messaging, {
+        vapidKey: env.NEXT_PUBLIC_FIREBASE_MESSAGING_VAPID_KEY,
+      })
+
+      //We can send token to server
+      console.log("Token generated : ", token)
+    } else if (permission === "denied") {
+      //notifications are blocked
+      alert("You denied for the notification")
+    }
+  }
+
+  useEffect(() => {
+    requestPermission()
+  }, [])
+
+  const notify = () => toast("Wow so easy!")
   return (
     <>
       <MainNavigaion />
@@ -39,9 +83,11 @@ export default function Web() {
             <Button href="https://github.com/tomek-i/whispers-in-the-dark" className="mr-3">
               Get started
             </Button>
+            <button onClick={notify}>Notify!</button>
           </div>
         </div>
       </section>
+      <ToastContainer />
     </>
   )
 }
