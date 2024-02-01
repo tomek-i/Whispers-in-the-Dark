983e989f67287aac7bfb8e5f73b8e66c9576eb7b
tomek-i <tomek.iwainski@gmail.com>
2024-02-01 20:59:23 +1100

    removed unnecessary config

--- src/env/server.env.ts
+++ src/env/server.env.ts
@@ -1,11 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
 
 export const env = createEnv({
-  server: {
-    FIREBASE_DATABASE_URL: z.string().url(),
-  },
-  runtimeEnv: {
-    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
-  },
+  server: {},
+  runtimeEnv: {},
 })
