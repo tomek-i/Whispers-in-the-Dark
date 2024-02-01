13b2570f857ab766b592054ab2e0de952f2d3eb1
tomek-i <tomek.iwainski@gmail.com>
2024-02-02 00:06:20 +1100

    fix imports

--- src/lib/auth/signin.ts
+++ src/lib/auth/signin.ts
@@ -1,8 +1,11 @@
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { firebase } from "../firebase"
+
 export default async function signIn(email: string, password: string) {
   let result = null,
     error = null
   try {
-    result = await signInWithEmailAndPassword(auth, email, password)
+    result = await signInWithEmailAndPassword(firebase.auth, email, password)
   } catch (e) {
     error = e
   }
