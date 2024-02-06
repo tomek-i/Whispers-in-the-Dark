aabe2014a89319841886168d123e38ce7dfbf4ea
tomek-i <tomek.iwainski@gmail.com>
2024-02-07 02:56:12 +1100

    Update route.ts

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,21 +1,41 @@
 import { NextRequest } from "next/server"
-import Pusher from "pusher"
-import { env as client } from "@/env/client.env"
-import { env as server } from "@/env/server.env"
+import { GameEventMessage } from "@/lib/game/messages"
+import { pusher } from "@/lib/pusher"
 
 export async function POST(request: NextRequest) {
-  const { message } = (await request.json()) as { message: string }
-  const cfg = {
-    appId: client.NEXT_PUBLIC_PUSHER_APP_ID,
-    key: client.NEXT_PUBLIC_PUSHER_APP_KEY,
-    cluster: client.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
-    secret: server.PUSHER_APP_SECRET,
-    // useTLS: true,
-  }
-  const pusher = new Pusher(cfg)
+  const { data, event } = (await request.json()) as GameEventMessage
 
-  //TODO: is there a way to send a message directly to a specific client/user?
-  // Trigger the 'message' event on the 'whsipers-in-the-dark' in Pusher
-  await pusher.trigger("whsipers-in-the-dark", "message", "Response: " + message)
-  return Response.json({ status: 200, ok: true, data: "Message sent!", message })
+  //TODO: the channel "whsipers-in-the-dark" should be dynamic based on the gameId eg. game-123
+  //TODO: the event "message" should be replaced by the event passed in the request
+  //TODO: the data that is sent depends what event is being triggered
+  //      eg. event could be DEMON_KILLED_PLAYER -> Player A who is the Demon KILLED Player B
+  //      in that case we want to send a message to all users in the game
+  //      but in the case of the DEMON_DIED we want to send a message to the player who is the
+  //      SCARLET WOMAN only - so in that case we could use the message event SCARLET_WOMAN this way
+  //      only the scarlet woman would be notified
+  //NOTE: based on the userId we can send a message back to the sender
+  //NOTE: based on the gameId we can send a message to all users in the game
+  await pusher.trigger("whsipers-in-the-dark", "message", "Response as JSON: " + JSON.stringify(data))
+
+  //TODO: fixup response
+  return Response.json({ status: 200, ok: true, data: "Message sent!", message: data })
+}
+
+function OK(data: any) {
+  return new Response(JSON.stringify({ error: null, data }), {
+    headers: { "Content-Type": "application/json" },
+    status: 200,
+  })
+}
+function Created(data: any) {
+  return new Response(JSON.stringify({ error: null, data }), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  })
+}
+function NoContent(data: any) {
+  return new Response("", {
+    headers: { "Content-Type": "application/json" },
+    status: 204,
+  })
 }
