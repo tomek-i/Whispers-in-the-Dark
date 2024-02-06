d7ba500b2a019dfd2ba9a16e02f2d02bc73c63ef
tomek-i <tomek.iwainski@gmail.com>
2024-02-07 02:56:04 +1100

    Create http.ts

---
+++ src/lib/http.ts
@@ -0,0 +1,12 @@
+export async function post<T>(url: string, data: T) {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  if (!res.ok) {
+    console.error("failed to POST data")
+  }
+}
