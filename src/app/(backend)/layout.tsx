14747347b88ddf1945c9749f35a5b2238d02286b
tomek-i <tomek.iwainski@gmail.com>
2024-02-08 03:21:51 +1100

    removed menu

--- src/app/(backend)/layout.tsx
+++ src/app/(backend)/layout.tsx
@@ -1,15 +1,11 @@
 "use client"
 
-import { MainNavigaion } from "@/components/MainNavigaion"
 import { AuthContextProvider } from "@/context"
 
 export default function BackendLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <AuthContextProvider>
-        <MainNavigaion />
-        {children}
-      </AuthContextProvider>
+      <AuthContextProvider>{children}</AuthContextProvider>
     </>
   )
 }
