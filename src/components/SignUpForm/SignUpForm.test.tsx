57c245a650f21b59a735aab091ec7b8e7f5d096a
tomek-i <tomek.iwainski@gmail.com>
2024-02-11 02:56:26 +1100

    deleted

--- src/components/SignUpForm/SignUpForm.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import { SignUpForm } from "components/SignUpForm"
-
-test("renders", () => {
-  render(<SignUpForm />)
-  expect(screen.getByText("SignUpForm")).toBeInTheDocument()
-})
