d0f5cc16aae94b71f615f044848027a75ed0720a
tomek-i <tomek.iwainski@gmail.com>
2024-02-14 10:37:50 +1100

    Update layout.tsx

--- src/app/(backend)/layout.tsx
+++ src/app/(backend)/layout.tsx
@@ -2,9 +2,13 @@
 
 import { signOut } from "firebase/auth"
 import { useRouter } from "next/navigation"
+import { redirect } from "next/navigation"
+import { useLayoutEffect } from "react"
 import { MainNavigaion } from "@/components/MainNavigaion"
-import { AuthContextProvider } from "@/context"
+import { AuthContextProvider, useAuthContext } from "@/context"
 import { firebase } from "@/lib/firebase"
+
+//TODO: for protected routes check out  https://nextjs.org/docs/pages/building-your-application/authentication
 
 export default function BackendLayout({ children }: { children: React.ReactNode }) {
   return (
